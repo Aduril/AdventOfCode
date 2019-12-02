ead46f400cbf87ba91944285847b167b0b876f06
Peter Melinat <github@mailin.at>
2019-12-02 21:53:18 +0100

    added data file

--- lib/aoc/fuel_calculator.ex
+++ lib/aoc/fuel_calculator.ex
@@ -3,21 +3,26 @@ defmodule AOC.FuelCalculator do
   def calculate_fuel_requirements() do
     get_module_masses()
     |> Enum.reduce(0, &add_fuel_requirement/2)
+    # |> add_fuel_for_fuel()
   end
 
   def get_module_masses() do
-    HTTPoison.get!("https://adventofcode.com/2019/day/1/input", [Cookie: "session=53616c7465645f5f7af2f187bd151887a037eeed580c0de7453a7da71ea093b02ede43270781d26d653d007e90da85fa"])
-    |> Map.get(:body)
+    File.read!("data/day1.txt")
     |> String.split("\n")
     |> Enum.reject(fn x -> x == "" end)
     |> Enum.map(&String.to_integer/1)
   end
 
   def add_fuel_requirement(mass, fuel_sum) do
-    calculate_fuel_requirements_for_mass(mass) + fuel_sum
+    add_fuel_for_fuel(calculate_fuel_requirements_for_mass(mass)) + fuel_sum
   end
 
-  def calculate_fuel_requirements_for_mass(mass) do
-    Float.floor(mass/3) - 2
+  def calculate_fuel_requirements_for_mass(mass) when mass <= 2, do: 0
+  def calculate_fuel_requirements_for_mass(mass), do: Float.floor(mass/3) - 2
+
+  def add_fuel_for_fuel(mass) when mass <= 2, do: 0
+  def add_fuel_for_fuel(mass) do
+    IO.inspect(mass)
+    mass + add_fuel_for_fuel(calculate_fuel_requirements_for_mass(mass))
   end
 end
