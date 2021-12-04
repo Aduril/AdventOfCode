13e6d431a12a1b247fa6ee326b85a54b8c9eb2a9
Peter Melinat <github@mailin.at>
2021-12-05 00:01:32 +0100

    day4 solution

--- lib/aoc/submarine.ex
+++ lib/aoc/submarine.ex
@@ -1,4 +1,111 @@
 defmodule AOC.Submarine do
+
+
+  def play_bingo_board_to_lose() do
+    "data/2021-day4.txt"
+    |> file_to_line_list()
+    |> find_worst_board()
+    |> calculate_result()
+  end
+
+  def play_bingo_board_to_win() do
+    "data/2021-day4-test.txt"
+    |> file_to_line_list()
+    |> find_best_board()
+    |> calculate_result()
+  end
+
+  defp find_worst_board([moves | boards]) do
+    moves = String.split(moves, ",")
+    boards = boards |> build_boards()
+    find_worst_board(boards, moves)
+  end
+
+  defp find_worst_board(boards, moves) do
+    {winners, move, boards, moves} = play_game(boards, moves)
+    cond do
+      length(moves) == 0 -> {winners, move, boards, moves}
+      Enum.all?(boards, fn f -> f.board in winners end) -> {winners, move, boards, moves}
+      true -> boards |> Enum.reject(&(&1.board in winners)) |> find_worst_board(moves)
+    end
+  end
+
+  defp find_best_board([moves | boards]) do
+    moves = String.split(moves, ",")
+    boards
+    |> build_boards()
+    |> play_game(moves)
+  end
+
+  defp play_game({[], move, boards}, []), do: {nil, move, boards, []}
+  defp play_game({[], _, boards}, moves), do: play_game(boards, moves)
+  defp play_game({winners, move,  boards}, moves), do: {winners, move, boards, moves}
+  defp play_game(boards, [move | moves]), do: play_game(play_move(boards, move), moves)
+
+  defp play_move(boards, move) do
+    boards
+    |> Enum.map(&(mark_fields(&1, move)))
+    |> check_for_winner(move)
+  end
+
+  defp calculate_result({[winner | _], move, fields, moves}) do
+    fields
+    |> Enum.filter(fn f -> f.board == winner end)
+    |> Enum.filter(fn f -> f.marked == false end)
+    |> Enum.map(fn f -> String.to_integer(f.value) end)
+    |> Enum.sum()
+    |> Kernel.*(String.to_integer(move))
+  end
+
+  defp check_for_winner(fields, move) do
+    x_columns =
+      fields
+      |> Enum.filter(fn f -> f.marked == true end)
+      |> Enum.group_by(fn f -> "#{f.board}#{f.x}" end)
+      |> Enum.map(fn {k, v} -> {k, v} end)
+
+    y_columns =
+      fields
+      |> Enum.filter(fn f -> f.marked == true end)
+      |> Enum.group_by(fn f -> "#{f.board}#{f.y}" end)
+      |> Enum.map(fn {k, v} -> {k, v} end)
+
+    winners =
+      x_columns
+      |> Kernel.++(y_columns)
+      |> Enum.map(fn {_, val} -> get_winner_board(val) end)
+      |> Enum.reject(fn b -> is_nil(b) end)
+
+    {winners, move, fields}
+  end
+
+  defp get_winner_board(list) when length(list) < 5, do: nil
+  defp get_winner_board([f| _] = list) when length(list) == 5, do: f.board
+
+  defp mark_fields(field, move) do
+    Map.update(field, :marked, false, fn v -> v || field.value == move end)
+  end
+
+  defp build_boards(boards) do
+    boards
+    |> Enum.map(fn board -> board |> String.split(" ") |> Enum.reject(&(&1 == "")) end)
+    |> Enum.chunk_every(5)
+    |> Enum.with_index()
+    |> Enum.map(&build_board/1)
+    |> List.flatten()
+  end
+
+  defp build_board({lines, index}), do: build_lines(lines, index, 0)
+
+  defp build_lines([], _, _), do: []
+  defp build_lines([l | ls], b, y), do: [build_line(l, b, y, 0) | build_lines(ls, b, y + 1)]
+
+  defp build_line([], _, _ ,_), do: []
+  defp build_line([f | fs], b, y, x), do: [build_field(f, b, y, x)| build_line(fs, b, y, x + 1)]
+
+  defp build_field(val, board, y, x), do: %{ value: val, y: y, x: x, board: board, marked: false}
+
+
   def check_life_support() do
     "data/2021-day3.txt"
     |> file_to_line_list()
